67a0a27ffb122275bb78e781e1b2db1384312b65
Alexander Zimdahl <azimdahl91@gmail.com>
2025-05-06 17:16:47 -0400

    feat: update favicon and add new icons for improved branding and contact options

--- src/components/ContactButtons.tsx
+++ src/components/ContactButtons.tsx
@@ -1,3 +1,6 @@
+import envelope from "../assets/icons/envelope.svg"
+import darkmodeGithub from "../assets/icons/GitHub_Invertocat_Light.svg"
+
 export default function ContactButtons({hardSize}: {hardSize?: string}) {
   const size = hardSize ? hardSize : "size-8 lg:size-10"
 
@@ -7,10 +10,10 @@ export default function ContactButtons({hardSize}: {hardSize?: string}) {
         <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn"  />
       </a>
       <a href="https://github.com/AZimdahl" target="_blank" rel="external noopener noreferrer" className={`${size} hover:scale-115 transition-transform duration-300 ease-in-out`}>
-        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" className="rounded" />
+        <img src={darkmodeGithub} alt="GitHub" className="rounded" />
       </a>
       <a href="mailto:azimdahl91+portfolio@gmail.com" className={`${size} hover:scale-115 transition-transform duration-300 ease-in-out`}>
-        <img src="https://cdn-icons-png.flaticon.com/512/732/732200.png" alt="Email"  />
+        <img src={envelope} alt="Email" />
       </a>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
       <div className="relative z-10 flex flex-col items-end h-full px-4 2xl:px-20 pt-18 text-white text-shadow-lg/80 bg-gradient-to-r from-transparent from-40% to-gray-900/50">
         <h1 className="text-4xl md:text-6xl text-right font-bold lg:ml-64">{randomGreeting}, I'm Alexander Zimdahl</h1>
         <h2 className="text-3xl md:text-4xl mt-4 md:mt-10">Software Engineer</h2>
-        <div className="block text-xl md:text-2xl 2xl:text-3xl max-w-xl lg:max-w-lg xl:max-w-2xl 2xl:max-w-3xl md:text-right mt-64 md:mt-56 lg:mt-32 xl:mt-52">
+        <div className="block md:text-right text-xl md:text-2xl 2xl:text-3xl max-w-xl lg:max-w-lg xl:max-w-2xl 2xl:max-w-3xl mt-64 md:mt-56 lg:mt-32 xl:mt-52">
           <p className="mb-5">I'm a Software Development Engineer focused on building clean, scalable, and user-centered web applications. With nearly five years of experience across front-end and back-end technologies, I bring a thoughtful, problem-solving approach to every project.</p>
           <p>Take a look around to see what I've been working on.</p>
         </div>
